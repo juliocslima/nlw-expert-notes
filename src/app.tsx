25645e50c5b87ca6e64f36cf17dd9facfdc3d01d
Julio Lima <lima.julio.cs@gmail.com>
2024-02-06 11:06:49 -0300

    Upgrading new card and card components - class 2

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,14 @@
 import logo from './assets/logo-nlw-expert.svg'
+
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
 export function App() {
+
+  const note = {
+    date: new Date(),
+    content: 'Hello world!'
+  }
   
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -19,9 +25,7 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note}/>
       </div>
     </div>
   )
